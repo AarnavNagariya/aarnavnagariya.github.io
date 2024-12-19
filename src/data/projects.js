d80462ddbb64cd1010be838d8cbcafa13e0b2a81
Aarnav Nagariya <74650049+AarnavNagariya@users.noreply.github.com>
2024-12-19 19:22:30 +0530

    Projects added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,121 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
+    title: 'VR-Phore',
+    subtitle: 'VR based alternative for Synoptophore',
     image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    date: '2024-12-04',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'Innovated a simulation of the synoptophore device using the Unity engine'
+      + 'to diagnose binocular vision problems such as strabismus and amblyopia'
+      + 'in a virtual reality headset. Granted a patent for this innovation',
+  },
+  {
+    title: 'Ray Optics Simulation',
+    subtitle: 'Research Project',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2024-09-01',
+    desc:
+      'Created an interactive ray optics simulation in Unity engine for children,'
+      + ' featuring distance and degree annotations, VIBGYOR diffraction simulation,'
+      + ' ray manipulation for image formation, and a game-like structure for easier learning.',
+  },
+  {
+    title: 'G-Reddit',
+    subtitle: 'Reddit Clone',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2023-05-01',
+    desc:
+      'A social media website, similar to that of Reddit with all the necessary functionalities'
+      + ' implemented in MERN stack along with dockerized code.',
+  },
+  {
+    title: '64-bit Y86 Processor Architecture',
+    subtitle: 'Custom 64-bit Y86 Processor',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2023-09-01',
+    desc:
+      'Designed a 64-bit Y86 architecture processor in Verilog, and'
+      + ' implemented a 5-stage pipeline for enhanced speed and efficiency.'
+      + ' Verified correctness in GTKWave.',
+  },
+  {
+    title: 'A-Shell',
+    subtitle: 'Custom Shell',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2022-11-01',
+    desc:
+      'A functional Linux shell using C programming language. '
+      + 'Developed and tested various commands with input and output redirection,'
+      + ' including both built-in and custom commands such as ls, cd, pwd, discover,'
+      + ' and history.',
+  },
+  {
+    title: 'E-Commerce Product Recommendation system',
+    subtitle: 'Product Recommendation System',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2024-08-01',
+    desc:
+      'Product recommendation system using NLP and Retrieval-Augmented Generation (RAG)'
+      + ' techniques backed by llms, Enabling user query understanding and provided relevant'
+      + ' suggestions based on preferences like department, category, brand, and price range '
+      + 'by the semantic meaning.',
+  },
+  {
+    title: 'Self-Adaptive ML based circuit',
+    subtitle: 'Autonomously Adaptive Circuit',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2022-03-01',
+    desc:
+      'Leveraged deep learning and Recurrent Neural Networks (RNNs) to model'
+      + 'dynamic responses to physical conditions.'
+      + ' Designed circuits that autonomously adapt to varying conditions.',
+  },
+  {
+    title: 'Decoding Mental Processes from FMRI images',
+    subtitle: 'Neural Network based prediction model for FMRI images',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2024-05-01',
+    desc:
+      'Utilized an existing neural network model (by replicating the following '
+      + 'research paper) and made custom autoencoder embeddings model for decoding '
+      + 'mental processes such as motor functions, speech, and emotions from fMRI images.'
+      + ' Achieved 88% accuracy from the autoencoder model.',
+  },
+  {
+    title: 'Contextual Text Clustering and labelling',
+    subtitle: 'BERT based Text Clustering',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2024-10-01',
+    desc:
+      'Developed a text clustering system using BERT embedding and K-Means algorithm'
+      + ' to analyze and categorize textual data, further from an annotated dataset of'
+      + ' sentences and their specific labels, fine-tuned mistral-7B for the specific task'
+      + ' of label classification and classified the labels for clusters.',
+  },
+  {
+    title: 'RAG based Document Formatter',
+    subtitle: 'Formatting Documents using Gemini',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2024-11-01',
+    desc:
+      'Developed a document formatting system using Retrieval-Augmented Generation (RAG)'
+      + ' techniques to generate a formatted document from a given text input.'
+      + ' The system is capable of generating formatted documents in various formats'
+      + ' such as PDF, HTML, and DOCX.',
+  },
+  {
+    title: 'Image Recoloring using Color Clustering and Harmonization',
+    subtitle: 'GMM based Photo Recoloring and Harmonization',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2024-12-01',
+    desc:
+      'Developed a photo recoloring system using Gaussian Mixture Models (GMM)'
+      + ' to cluster colors in an image to generate a color palette.'
+      + ' Then recoloring the cluster centers to desired colors. '
+      + ' These desired color can be extracted from another image as well. '
+      + ' Also creatred a harmonization system from previous studies to harmonize the image'
+      + 'and extended these functionalities to video recoloring and harmonization.',
   },
 ];
 
